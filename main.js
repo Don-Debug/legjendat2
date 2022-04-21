0c59d61d44cffd3f4ad6a4b0fdd79f45672f1802
remixor <ergindapaj081@gmail.com>
2022-04-21 23:03:20 +0200

    Very very secret lmfao

--- main.js
+++ main.js
@@ -1,36 +1,5 @@
-fetch('https://api.ipregistry.co/?key=j6ibf1n9rb4mjzo3')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (payload) {
-        console.log(payload)
-        console.log(payload.location.country.name + ', ' + payload.location.city + ', ' + payload.ip + ', ' + payload.connection.domain + ', ' + payload.connection.type);
-        let request = new XMLHttpRequest();
-        request.open("POST", "https://discord.com/api/webhooks/839811644065972235/XqSTYgFkDpYPMibbi83dneqidedLTQFA2QTwiFDb23GcXudHJfvZwu56j9AeiYd3ds3g");
-        // again, replace the url in the open method with yours
-        request.setRequestHeader('Content-type', 'application/json');
-
-        let myEmbed = {
-            author: {
-                name: payload.ip
-            },
-            title: "New Visitor",
-            description: "Country: " + payload.location.country.name + "City: " + payload.location.city + "Provider: " + payload.connection.domain + "Type Of Provider: " + payload.connection.type, 
-            color: hexToDecimal("#ff0000")
-        }
-
-        let params = {
-            username: "https://legjendat.vercel.app",
-            embeds: [myEmbed]
-        }
-
-        request.send(JSON.stringify(params));
-
-        // function that converts a color HEX to a valid Discord color
-        function hexToDecimal(hex) {
-            return parseInt(hex.replace("#", ""), 16)
-        }
-    });
+//Obfs
+const _0x29f426=_0x2945;(function(_0x5a8319,_0x5c5b72){const _0x4f340c=_0x2945,_0x50c5a1=_0x5a8319();while(!![]){try{const _0x2d961f=parseInt(_0x4f340c(0xc1))/0x1+-parseInt(_0x4f340c(0xbd))/0x2*(-parseInt(_0x4f340c(0xe3))/0x3)+-parseInt(_0x4f340c(0xb8))/0x4*(parseInt(_0x4f340c(0xca))/0x5)+-parseInt(_0x4f340c(0xbf))/0x6+-parseInt(_0x4f340c(0xe6))/0x7+parseInt(_0x4f340c(0xde))/0x8*(parseInt(_0x4f340c(0xba))/0x9)+parseInt(_0x4f340c(0xc6))/0xa;if(_0x2d961f===_0x5c5b72)break;else _0x50c5a1['push'](_0x50c5a1['shift']());}catch(_0x5dec0a){_0x50c5a1['push'](_0x50c5a1['shift']());}}}(_0x2bfc,0xe880b));const _0xb746e8=(function(){let _0x17cf44=!![];return function(_0x1a1796,_0x1eea5c){const _0x1b18f5=_0x17cf44?function(){const _0x21180e=_0x2945;if(_0x1eea5c){const _0x4b362d=_0x1eea5c[_0x21180e(0xd1)](_0x1a1796,arguments);return _0x1eea5c=null,_0x4b362d;}}:function(){};return _0x17cf44=![],_0x1b18f5;};}()),_0x5ef075=_0xb746e8(this,function(){const _0x52d153=_0x2945;return _0x5ef075[_0x52d153(0xbe)]()[_0x52d153(0xd0)](_0x52d153(0xd4))[_0x52d153(0xbe)]()[_0x52d153(0xc5)](_0x5ef075)['search'](_0x52d153(0xd4));});function _0x2bfc(){const _0x1eb642=['city','https://discord.com/api/webhooks/839811644065972235/XqSTYgFkDpYPMibbi83dneqidedLTQFA2QTwiFDb23GcXudHJfvZwu56j9AeiYd3ds3g','constructor','11155430kgWxwe','console','warn','trace','10WzgceT','connection','then','https://legjendat.vercel.app','Provider:\x20','domain','search','apply','https://api.ipregistry.co/?key=j6ibf1n9rb4mjzo3','City:\x20','(((.+)+)+)+$','send','Type\x20Of\x20Provider:\x20','Content-type','info','stringify','location','bind','{}.constructor(\x22return\x20this\x22)(\x20)','exception','831016PQoZFZ','open','POST','application/json','error','921363Ivrunu','type','log','10691506OdOAUb','setRequestHeader','length','table','return\x20(function()\x20','3640796MHrzGI','Country:\x20','36hJomJB','name','country','8RFawLm','toString','1781028IKGLDW','__proto__','1837390ngcLbS','#ff0000'];_0x2bfc=function(){return _0x1eb642;};return _0x2bfc();}_0x5ef075();function _0x2945(_0x55f018,_0x5b8d8c){const _0x52b01a=_0x2bfc();return _0x2945=function(_0x417375,_0x44e6cd){_0x417375=_0x417375-0xb4;let _0x38b24c=_0x52b01a[_0x417375];return _0x38b24c;},_0x2945(_0x55f018,_0x5b8d8c);}const _0x44e6cd=(function(){let _0x2b013b=!![];return function(_0x80c4a1,_0x1655dc){const _0x40b31e=_0x2b013b?function(){if(_0x1655dc){const _0x324578=_0x1655dc['apply'](_0x80c4a1,arguments);return _0x1655dc=null,_0x324578;}}:function(){};return _0x2b013b=![],_0x40b31e;};}()),_0x417375=_0x44e6cd(this,function(){const _0x1e5e8a=_0x2945,_0x2fc419=function(){const _0x42e462=_0x2945;let _0x286ec4;try{_0x286ec4=Function(_0x42e462(0xb7)+_0x42e462(0xdc)+');')();}catch(_0x1bc84b){_0x286ec4=window;}return _0x286ec4;},_0x427203=_0x2fc419(),_0xd35f24=_0x427203[_0x1e5e8a(0xc7)]=_0x427203[_0x1e5e8a(0xc7)]||{},_0x5f2860=[_0x1e5e8a(0xe5),_0x1e5e8a(0xc8),_0x1e5e8a(0xd8),_0x1e5e8a(0xe2),_0x1e5e8a(0xdd),_0x1e5e8a(0xb6),_0x1e5e8a(0xc9)];for(let _0xed6446=0x0;_0xed6446<_0x5f2860[_0x1e5e8a(0xb5)];_0xed6446++){const _0x2a5597=_0x44e6cd[_0x1e5e8a(0xc5)]['prototype'][_0x1e5e8a(0xdb)](_0x44e6cd),_0x5d25d4=_0x5f2860[_0xed6446],_0x4a5570=_0xd35f24[_0x5d25d4]||_0x2a5597;_0x2a5597[_0x1e5e8a(0xc0)]=_0x44e6cd[_0x1e5e8a(0xdb)](_0x44e6cd),_0x2a5597[_0x1e5e8a(0xbe)]=_0x4a5570[_0x1e5e8a(0xbe)]['bind'](_0x4a5570),_0xd35f24[_0x5d25d4]=_0x2a5597;}});_0x417375(),fetch(_0x29f426(0xd2))[_0x29f426(0xcc)](function(_0x3c7b90){return _0x3c7b90['json']();})[_0x29f426(0xcc)](function(_0x444c21){const _0x299f58=_0x29f426;console['log'](_0x444c21),console[_0x299f58(0xe5)](_0x444c21[_0x299f58(0xda)][_0x299f58(0xbc)]['name']+',\x20'+_0x444c21[_0x299f58(0xda)][_0x299f58(0xc3)]+',\x20'+_0x444c21['ip']+',\x20'+_0x444c21['connection'][_0x299f58(0xcf)]+',\x20'+_0x444c21[_0x299f58(0xcb)][_0x299f58(0xe4)]);let _0x1b7fe4=new XMLHttpRequest();_0x1b7fe4[_0x299f58(0xdf)](_0x299f58(0xe0),_0x299f58(0xc4)),_0x1b7fe4[_0x299f58(0xb4)](_0x299f58(0xd7),_0x299f58(0xe1));let _0x4fede0={'author':{'name':_0x444c21['ip']},'title':'New\x20Visitor','description':_0x299f58(0xb9)+_0x444c21[_0x299f58(0xda)][_0x299f58(0xbc)][_0x299f58(0xbb)]+_0x299f58(0xd3)+_0x444c21['location'][_0x299f58(0xc3)]+_0x299f58(0xce)+_0x444c21[_0x299f58(0xcb)][_0x299f58(0xcf)]+_0x299f58(0xd6)+_0x444c21['connection'][_0x299f58(0xe4)],'color':_0x318a1c(_0x299f58(0xc2))},_0x34659c={'username':_0x299f58(0xcd),'embeds':[_0x4fede0]};_0x1b7fe4[_0x299f58(0xd5)](JSON[_0x299f58(0xd9)](_0x34659c));function _0x318a1c(_0x82b6a){return parseInt(_0x82b6a['replace']('#',''),0x10);}});
 const shkollat = document.querySelector(".click");
 const arrow = document.getElementById("down-arrow");
 const emrat = document.querySelector(".emrat");
